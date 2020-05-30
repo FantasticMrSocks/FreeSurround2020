fb6b4c040fe3bd972c67d5654ee87a290d8f91aa
Brian Barnes <brian@livin-in-the-database.hsd1.nh.comcast.net>
2020-05-30 11:48:57 -0400

    Put the buffers back!

--- pcm_freesurround2020.cpp
+++ pcm_freesurround2020.cpp
@@ -293,18 +293,15 @@ static snd_pcm_sframes_t fs_transfer(snd_pcm_extplug_t *ext,
             src[c] += src_step[c];
         }
     }
-    //data->in_buf->multipush(in_vec);
-    float in_arr[in_vec.size()];
-    std::copy(in_vec.begin(),in_vec.end(),in_arr);
-    data->plugin->get_chunk(in_arr, in_vec.size());
+    data->in_buf->multipush(in_vec);
 
     // Copy from output buffer into dst
-    std::vector<float> out_vec = data->plugin->get_out_buf(); //data->out_buf->multipop(size*OUTPUT_CHANNELS);
+    std::vector<float> out_vec = data->out_buf->multipop(size*OUTPUT_CHANNELS);
     int i=0;
     for (s=0; s<size; s++) {
         for (c=0; c<OUTPUT_CHANNELS; c++) {
             //*dst[c] = data->out_buf->pop();
-            if (i < out_vec.size()) {*dst[c] = out_vec[i];} else {*dst[c] = 0.0;}
+            if (out_vec.size()) {*dst[c] = out_vec[i];} else {*dst[c] = 0.0;}
             dst[c] += dst_step[c];
             i++;
         }
@@ -323,7 +320,7 @@ static int fs_prepare(snd_pcm_extplug_t *ext) {
     fs_data *data = (fs_data *)ext->private_data;
     data->in_buf = new circ_buffer<float>(1000000, 0.0);
     data->out_buf = new circ_buffer<float>(1000000, 0.0);
-   // fs_thread = new std::thread(decode_thread, data);
+    fs_thread = new std::thread(decode_thread, data);
     return 0;
 }
 
@@ -333,8 +330,8 @@ static int fs_prepare(snd_pcm_extplug_t *ext) {
 static int fs_close(snd_pcm_extplug_t *ext) {
     fs_data *data = (fs_data *)ext->private_data;
     data->finish = true;
-   // fs_thread->join();
-   // delete fs_thread;
+    fs_thread->join();
+    delete fs_thread;
     delete data->plugin;
     delete data->in_buf;
     delete data->out_buf;
