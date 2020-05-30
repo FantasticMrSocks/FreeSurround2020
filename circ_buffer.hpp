66e9ac11ae775f82bfa6d2de5f2c026349bfcf89
Brian Barnes <brian@livin-in-the-database.hsd1.nh.comcast.net>
2020-05-30 00:56:11 -0400

    Experiment to make multipop wait for number

--- circ_buffer.hpp
+++ circ_buffer.hpp
@@ -1,6 +1,7 @@
 #include <boost/circular_buffer.hpp>
 #include <mutex>
 #include <vector>
+#include <iostream>
 
 template <typename T>
 class circ_buffer
@@ -13,15 +14,15 @@ public:
     }
     void push (T data) {
         buf_mx.lock();
-        //std::cout << "Lock acquired in push\n";
+        // std::cout << "Lock acquired in push\n";
         buf.push_back(data);
         buf_mx.unlock();
-        //std::cout << "    Lock released in push\n";
+        // std::cout << "    Lock released in push\n";
     }
     T pop() {
         T data;
         buf_mx.lock();
-        //std::cout << "Lock acquired in pop\n";
+        // std::cout << "Lock acquired in pop\n";
         if (buf.empty()) {
             data = NULL;
         } else {
@@ -29,25 +30,30 @@ public:
             buf.pop_front();
         }
         buf_mx.unlock();
-        //std::cout << "    Lock released in pop\n";
+        // std::cout << "    Lock released in pop\n";
         return data;
     }
     void multipush(std::vector<T> data) {
         buf_mx.lock();
-        //std::cout << "Lock acquired in multipush\n";
+        // std::cout << "Lock acquired in multipush\n";
         buf.insert(buf.end(), data.begin(), data.end());
         buf_mx.unlock();
-        //std::cout << "    Lock released in multipush\n";
+        // std::cout << "    Lock released in multipush\n";
     }
     std::vector<T> multipop() {
         std::vector<T> data;
         buf_mx.lock();
-        //std::cout << "Lock acquired in multipop\n";
+        // std::cout << "Lock acquired in multipop\n";
         data.insert(data.begin(), buf.begin(), buf.end());
         buf.clear();
         buf_mx.unlock();
-        //std::cout << "    Lock released in multipop\n";
+        // std::cout << "    Lock released in multipop\n";
         return data;
+    }
+    std::vector<T> multipop(int n) {
+        std::vector<T> data;
+        while (buf.size() < n) {}
+        data = multipop();
     }
     int size() {
         int size;
@@ -56,17 +62,17 @@ public:
     }
     void resize(int capacity, T val) {
         buf_mx.lock();
-        //std::cout << "Lock acquired in resize (val)\n";
+        // std::cout << "Lock acquired in resize (val)\n";
         buf.resize(capacity, val);
         buf_mx.unlock();
-        //std::cout << "    Lock released in resize (val)\n";
+        // std::cout << "    Lock released in resize (val)\n";
     }
     void resize(int capacity) {
         buf_mx.lock();
-        //std::cout << "Lock acquired in resize (no val)\n";
+        // std::cout << "Lock acquired in resize (no val)\n";
         buf.resize(capacity, default_value);
         buf_mx.unlock();
-        //std::cout << "    Lock released in resize (no val)\n";
+        // std::cout << "    Lock released in resize (no val)\n";
     }
     void set_capacity(int capacity) {
         buf_mx.lock();
